0b5817c3937d6ec7400785b950e0fefaf6c93952
Sandeep <sandeepchowdarymallineni007@gmail.com>
2019-12-29 20:36:24 +0530

    Initial commit

---
+++ backend/routes/users.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+router.route('/').get((req, res) => {
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+  
+User.create({username : req.body.username})
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
